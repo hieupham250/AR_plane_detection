1360c0a8048d5ce41d216746fc04ddd71f12f9a7
hieupham250 <ptrunghieu2507@gmail.com>
2025-07-04 11:22:21 +0700

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  base: "/AR_plane_detection/",
   plugins: [react(), basicSsl()],
   server: {
     https: true,
